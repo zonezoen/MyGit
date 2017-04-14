28c26c7f8ede6de422916ca06c89d214d2fe4de9
zonegit <842221695@qq.com>
2017-04-14 15:40:01 +0800

    add feature1 text on feature1 branch

--- app/src/main/java/com/zone/mygit/MainActivity.java
+++ app/src/main/java/com/zone/mygit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.tv);
-        textView.setText("feature1");
+        textView.setText("feature11");
 
 
     }
