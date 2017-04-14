f3a3858943e56cb75037e8db9d67ffeb692ec82a
zonegit <842221695@qq.com>
2017-04-14 15:42:50 +0800

    add Log text on feature1 branch

--- app/src/main/java/com/zone/mygit/MainActivity.java
+++ app/src/main/java/com/zone/mygit/MainActivity.java
@@ -2,17 +2,18 @@ package com.zone.mygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG = "zoneLog";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.tv);
         textView.setText("feature11");
-
+        Log.d(TAG, "onCreate: asdf");
 
     }
 }
